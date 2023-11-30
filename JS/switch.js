d1a188dff8d6eae1c97d143efb6bcd29bc98da7e
Mr Groot <113226362+MrGrootx@users.noreply.github.com>
2023-11-30 18:24:06 +0530

    javascript

--- JS/switch.js
+++ JS/switch.js
@@ -1,15 +1,20 @@
-const chocolateName = "Toblerone";
+const chocolateName = 1;
+console.log(typeof chocolateName);
 
-const OurStocks = ["Toblerone", "Milka", "Twix", "lindt", "slice", "Cadbury"];
-console.log(OurStocks);
+if (typeof chocolateName == "number") {
+  console.log("You Can't Type in Number");
+} else {
+  const OurStocks = ["Toblerone", "Milka", "Twix", "lindt", "slice", "Cadbury"];
+  console.log(OurStocks);
 
-const stock = OurStocks.some((stk) => chocolateName.includes(stk));
-console.log(stock);
+  const stock = OurStocks.some((stk) => chocolateName.includes(stk));
+  console.log(stock);
 
-switch (true) {
-  case stock:
-    console.log(`${chocolateName} is Available`);
-    break;
-  default:
-    console.log(`${chocolateName} Out Of Stock`);
+  switch (true) {
+    case stock:
+      console.log(`${chocolateName} is Available`);
+      break;
+    default:
+      console.log(`${chocolateName} Out Of Stock`);
+  }
 }
