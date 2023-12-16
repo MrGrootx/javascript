a09cce7d9afe8b582490ff38ec7624f3889c309e
Mr Groot <113226362+MrGrootx@users.noreply.github.com>
2023-12-16 20:25:58 +0530

    javascript

--- JS/foreach.js
+++ JS/foreach.js
@@ -1,7 +1,17 @@
-
+/*
 const Foods = ["Burger", "Pizza", "Donut"]
 
 Foods.forEach((food) => {
     console.log(food);
 })
 
+*/
+
+const names = ["groot", "Abishek", "Flash", "Alex"];
+
+names.forEach((name, index) => {
+    console.log(name, index);
+    names[index] = name.toUpperCase()
+  
+})  
+console.log(name);
