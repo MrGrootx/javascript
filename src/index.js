2d171c0959260fea548375f24f26d0b8ef8a25c7
Mr Groot <113226362+MrGrootx@users.noreply.github.com>
2024-01-23 22:15:47 +0530

    javascript

--- src/index.js
+++ src/index.js
@@ -10,3 +10,7 @@ const Names = [
 ];
 
 // console.log(fetch("Gkz2HQKfPH7KoNh2K1SqFcz24xz92YYB"));
+
+const userName = "MrGrootx"
+
+console.log(fetch(`https://api.github.com/users/${userName}`).then(res => console.log(res.json())).then(data => console.log(data)));
